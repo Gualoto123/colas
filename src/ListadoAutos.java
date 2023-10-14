97e5730c664f1107b6d5a663e7fdd7226c4384aa
Edwin11 <edwin.gualoto.quilumba@udla.edu.ec>
2023-10-14 10:55:51 -0500

    colas

---
+++ src/ListadoAutos.java
@@ -0,0 +1,29 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ListadoAutos {
+
+    private Queue <Auto> listadoAutos;
+
+    public ListadoAutos() {
+        listadoAutos = new LinkedList<Auto>();
+    }
+
+    public void encolar (Auto dato){
+
+        listadoAutos.add(dato);
+    }
+    public Auto desencolar() throws Exception {
+        if (listadoAutos.isEmpty())
+           throw new Exception("No hay elementos en la fila");
+        return listadoAutos.remove();
+    }
+    public String listarAutos(){
+        String resultado="";
+        for (Auto auto:listadoAutos) {
+            resultado+= auto.toString();
+        }
+        return listadoAutos.size()>0?resultado:"No hay elementos";
+    }
+
+}
